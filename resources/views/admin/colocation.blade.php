9ce1423550f52baad387a707ec6a156e1508ea36
Ninjaa <ahlulffirdaus@gmail.com>
2023-08-29 15:38:28 +0700

    adsf

--- resources/views/admin/colocation.blade.php
+++ resources/views/admin/colocation.blade.php
@@ -9,31 +9,27 @@
                         <table class="min-w-full text-center text-sm font-light">
                             <thead class="border-b font-medium dark:border-neutral-500">
                                 <tr>
-                                    <th scope="col" class="px-6 py-2">#</th>
-                                    <th scope="col" class="px-6 py-2">First</th>
-                                    <th scope="col" class="px-6 py-2">Last</th>
-                                    <th scope="col" class="px-6 py-2">Handle</th>
+                                    <th scope="col" class="px-6 py-2">NO</th>
+                                    <th scope="col" class="px-6 py-2">INSTANSI</th>
+                                    <th scope="col" class="px-6 py-2">PENANGGUNG JAWAB</th>
+                                    <th scope="col" class="px-6 py-2">NIP</th>
+                                    <th scope="col" class="px-6 py-2">STATUS</th>
+                                    <th scope="col" class="px-6 py-2">AKSI</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr class="border-b dark:border-neutral-500">
                                     <td class="whitespace-nowrap px-6 py-2 font-medium">1</td>
-                                    <td class="whitespace-nowrap px-6 py-2">Mark</td>
-                                    <td class="whitespace-nowrap px-6 py-2">Otto</td>
-                                    <td class="whitespace-nowrap px-6 py-2">@mdo</td>
-                                </tr>
-                                <tr class="border-b dark:border-neutral-500">
-                                    <td class="whitespace-nowrap px-6 py-2 font-medium">2</td>
-                                    <td class="whitespace-nowrap px-6 py-2 ">Jacob</td>
-                                    <td class="whitespace-nowrap px-6 py-2">Thornton</td>
-                                    <td class="whitespace-nowrap px-6 py-2">@fat</td>
-                                </tr>
-                                <tr class="border-b dark:border-neutral-500">
-                                    <td class="whitespace-nowrap px-6 py-2 font-medium">3</td>
-                                    <td colspan="2" class="whitespace-nowrap px-6 py-2">
-                                        Larry the Bird
+                                    <td class="whitespace-nowrap px-6 py-2">PT SEJAHTERA</td>
+                                    <td class="whitespace-nowrap px-6 py-2">John Doe</td>
+                                    <td class="whitespace-nowrap px-6 py-2">192839201829327183</td>
+                                    <td class="whitespace-nowrap px-6 py-2">Belum Disetujui</td>
+                                    <td class="whitespace-nowrap px-6 py-2">
+                                        <div class="flex gap-x-3">
+                                        <a href="#" class="h-[30px] w-[30px]"><img src="{{asset('assets/detail-icon.svg')}}"></button></a>
+                                        <a href="#" class="btn"><img src="{{asset('assets/detail-icon.svg')}}"></button></a>
+                                    </div>
                                     </td>
-                                    <td class="whitespace-nowrap px-6 py-2">@twitter</td>
                                 </tr>
                             </tbody>
                         </table>
