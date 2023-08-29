1d22de4f6104fe59a470960e0b2bc74c5a303aa2
Rkhyg <eyyyzx@gmail.com>
2023-08-29 14:07:33 +0700

    Surat-Pengantar-Pengajuan-Server
    Surat-Pengantar-Pengajuan-Server

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,10 @@ Route::get('/colocation', function () {
     return view('admin/colocation');
 });
 
+Route::get('/VPS/Surat-Pengantar-Pengajuan-Server', function () {
+    return view('Surat-Pengantar-Pengajuan-Server');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
