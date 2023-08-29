b04bf5ffaa7f3a4cdd7b63bae6fdbd8e264baffc
Ali Fahmi Yusuf <107609258+alifhmiysf@users.noreply.github.com>
2023-08-29 09:40:45 +0700

    a

---
+++ resources/views/layouts/dashboard.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,200;9..40,300;9..40,400;9..40,500;9..40,600&family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="font-sans">
+
+    @yield('sidebar')
+
+</body>
+</html>
---
+++ resources/views/sidebar.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.dashboard')
+
+@section('sidebar')
+
+@endsection
